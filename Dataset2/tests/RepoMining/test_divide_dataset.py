fe71ded442750cedd3b03edcd2be8f78bdfc8749
d-donia <d.donia@studenti.unisa.it>
2024-09-07 20:05:39 +0200

    Update test_case 2-3-4 in test_divide_dataset.py

--- Dataset2/tests/RepoMining/test_divide_dataset.py
+++ Dataset2/tests/RepoMining/test_divide_dataset.py
@@ -13,8 +13,12 @@ def divide_dataset_to_test():
     '''
     Divide the entire dataset in small pieces of 50 commits.
     '''
+    print("OS_CWD_BEFORE_CHDIR:", os.getcwd())
     os.chdir("..")
     cwd = os.getcwd()
+    print("OS_CWD_TESTF:", cwd)
+    print("OS_LISTDIR_TESTF:", os.listdir())
+
     csvfile = open('initial_Dataset.csv', 'r').readlines()
     filename = 1
     if "Dataset_Divided" not in os.listdir():
@@ -47,7 +51,7 @@ class TestDivideDataset:
 
     # FAILURE: NO RECORD MEANS DATASET_DIVIDED DIR EMPTY
     # 1.CSV CONTAINS HEADER OF THE DATASET
-    @pytest.mark.xfail
+
     @patch('os.getcwd', return_value=TEST_PATH)
     @patch('os.listdir', return_value=[DIR_NAME])
     @patch('os.chdir')
@@ -64,34 +68,33 @@ class TestDivideDataset:
 
             mock_files[self.DATASET_NAME].assert_called_with(self.DATASET_NAME, 'r')
 
-            mock_files['1.csv']().writelines.assert_not_called()
+            mock_files['1.csv']().writelines.assert_called_with([self.DATASET_HEADERS + '\n'])
 
     # FAILURE: ONLY 1st CSV contains headers
 
-    @pytest.mark.xfail
+
     @patch('os.getcwd', return_value=TEST_PATH)
     @patch('os.listdir', return_value=[DIR_NAME])
     @patch('os.chdir')
     @patch('os.mkdir')
     @pytest.mark.parametrize('mock_files', [
-        {DATASET_NAME: generate_csv_string(0), '1.csv': None}
+        {DATASET_NAME: generate_csv_string(50), '1.csv': None}
     ], indirect=True)
-    def test_case_2(self, mock_mkdir, mock_chdir, mock_listdir, mock_getcwd, mock_files):
-        divide_dataset_to_test()
+    @pytest.mark.parametrize('mock_op_permission_err', [
+        '1.csv'
+    ], indirect=True)
+    def test_case_3(self, mock_mkdir, mock_chdir, mock_listdir, mock_getcwd, mock_op_permission_err, mock_files):
 
-        mock_mkdir.assert_not_called()
-        mock_chdir.assert_called_with(self.TEST_PATH + "/" + self.DIR_NAME)
+        with pytest.raises(PermissionError):
+            divide_dataset_to_test()
 
-        mock_files[self.DATASET_NAME].assert_called_with(self.DATASET_NAME, 'r')
-
-        mock_files['1.csv']().writelines.assert_not_called()
 
     # FALLIMENTO: QUANDO NON HO RECORD MI ASPETTO DIVIDE_DATASET DIR VUOTA
     # ERRORE: 1.CSV CONTIENE GLI HEADER DI INITIAL DATASET (SONO PRESENTI SOLO IN QUESTO FILE)
 
     @pytest.mark.xfail
     @patch('os.getcwd', return_value=TEST_PATH)
-    @patch('os.listdir', return_value=[DIR_NAME])
+    @patch('os.listdir', return_value=[])
     @patch('os.chdir')
     @patch('os.mkdir')
     @pytest.mark.parametrize('mock_files', [
@@ -100,7 +103,7 @@ class TestDivideDataset:
     def test_case_4(self, mock_mkdir, mock_chdir, mock_listdir, mock_getcwd, mock_files):
         divide_dataset_to_test()
 
-        mock_mkdir.assert_not_called()
+        mock_mkdir.assert_called_once_with(self.DIR_NAME)
         mock_chdir.assert_called_with(self.TEST_PATH + "/" + self.DIR_NAME)
 
         mock_files[self.DATASET_NAME].assert_called_with(self.DATASET_NAME, 'r')
@@ -109,18 +112,26 @@ class TestDivideDataset:
 
         oracle_data = generate_csv_string(60)
 
-        lines = oracle_data.strip().split('\r\n')  # Use '\r\n' to split lines
-        lines = [line + "\r\n" for line in lines[1:]]
+        lines = oracle_data.strip().split('\n')  # Use '\r\n' to split lines
+
+        print("BEFORE:", lines)
+
+        lines = [line + "\n" for line in lines[1:]]
+
+        print("AFTER:", lines)
 
         lines_per_dataset = 50
 
-        for i, dataset in enumerate(oracle_datasets):
+        print("LUN:", len(oracle_datasets))
+
+        for i in range(len(oracle_datasets)):
+            print("START INDEX: ", i)
             start_index = i * lines_per_dataset
             end_index = start_index + lines_per_dataset
             expected_lines = lines[start_index:end_index]
 
-            mock_files[dataset]().write.assert_called_with(self.DATASET_HEADERS)
-            mock_files[dataset]().writelines.assert_called_with(expected_lines)
+            mock_files[oracle_datasets[i]]().write.assert_called_with(self.DATASET_HEADERS + '\n')
+            mock_files[oracle_datasets[i]]().writelines.assert_called_with(expected_lines)
 
 
 
