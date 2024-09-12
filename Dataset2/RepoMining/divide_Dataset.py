24769c000b6c2df6bc10d62946e3f42b0f45a030
d-donia <d.donia@studenti.unisa.it>
2024-09-12 23:44:52 +0200

    Fix in test_divide_dataset.py and divide_Dataset.py

--- Dataset2/RepoMining/divide_Dataset.py
+++ Dataset2/RepoMining/divide_Dataset.py
@@ -8,8 +8,18 @@ csvfile = open('initial_Dataset.csv', 'r').readlines()
 filename = 1
 if "Dataset_Divided" not in os.listdir():
 	os.mkdir("Dataset_Divided")
-os.chdir(cwd +"/Dataset_Divided")
-for i in range(len(csvfile)):
-	if i % 50 == 0:
-		open(str(filename) + '.csv', 'w+').writelines(csvfile[i:i+50])
-		filename += 1
+os.chdir(cwd + "/Dataset_Divided")
+header = csvfile[0]
+
+csvfile = csvfile[1:]
+
+if len(csvfile)==0:
+	with open(str(filename) + '.csv', 'w+') as new_file:
+		new_file.writelines([header])
+else:
+	for i in range(0, len(csvfile)):
+		if i % 50 == 0:
+			with open(str(filename) + '.csv', 'w+') as new_file:
+				new_file.write(header)
+				new_file.writelines(csvfile[i:i + 50])
+			filename += 1
