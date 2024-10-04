2b0479cf3de47df29d46b771ec28933b7902e33d
d-donia <d.donia@studenti.unisa.it>
2024-10-04 23:08:16 +0200

    Add essential complexity metrics in SoftwareMetrics.py

--- Dataset2/Software_Metrics/SoftwareMetrics.py
+++ Dataset2/Software_Metrics/SoftwareMetrics.py
@@ -274,6 +274,254 @@ class SoftwareMetrics:
                     count += 1
         return count
 
+
+
+    def compute_complexity(self, java_code):
+        max_essential_complexity = 1
+        sum_essential_complexity = 0
+        decision_points = 0
+
+        def is_reducible_stat(node):
+            return not isinstance(node, (
+                javalang.tree.ReturnStatement, javalang.tree.BreakStatement, javalang.tree.ContinueStatement))
+
+        def is_control_stat(node):
+            return isinstance(node, (
+                javalang.tree.IfStatement, javalang.tree.WhileStatement, javalang.tree.DoStatement,
+                javalang.tree.ForStatement,
+                javalang.tree.SwitchStatement, javalang.tree.TryStatement))
+
+        def is_case_reducible(case):
+            is_break_present = False
+            is_case_reducible = True
+
+            for statement in case.statements:
+                if isinstance(statement, javalang.tree.Statement):
+                    reduce_node(statement)
+                    if not isinstance(statement, javalang.tree.BreakStatement) and not statement.is_reducible:
+                        is_case_reducible = is_case_reducible and False
+                    else:
+                        if isinstance(statement, javalang.tree.BreakStatement):
+                            is_break_present = True
+
+            if not is_break_present:
+                is_case_reducible = False
+
+            return is_case_reducible
+
+        def is_catches_reducible(catches):
+            not_reducible_statements_in_catches = []
+            catches_reducibilities = []
+            all_same = True
+
+            for catch in catches:
+                catch.is_reducible = True
+                not_reducible_types = set()
+                for statement in catch.block:
+                    if isinstance(statement, javalang.tree.Statement):
+                        reduce_node(statement)
+                        if not statement.is_reducible:
+                            if not is_reducible_stat(statement):
+                                not_reducible_types.add(statement.__class__.__name__)
+                                if (len(not_reducible_types) > 1):
+                                    all_same = False
+                            else:
+                                catch.is_reducible = False
+                catches_reducibilities.append(catch.is_reducible)
+                not_reducible_statements_in_catches.append(not_reducible_types)
+
+            if (all_same):
+                all_same = all(
+                    st == not_reducible_statements_in_catches[0] for st in not_reducible_statements_in_catches)
+
+            if (len(not_reducible_statements_in_catches) != 0 and not all_same) or (False in catches_reducibilities):
+                for catch in catches:
+                    catch.is_reducible = False
+                return False
+
+            return True
+
+        def reduce_node(node):
+            if node is not None:
+                if isinstance(node, javalang.tree.CompilationUnit):
+                    for type in node.types:
+                        reduce_node(type)
+
+                if isinstance(node, javalang.tree.ClassDeclaration) and node.body is not None:
+                    for declaration in node.body:
+                        reduce_node(declaration)
+
+                if isinstance(node, javalang.tree.ClassCreator):
+                    if node.body is not None:
+                        for method in node.body:
+                            reduce_node(method)
+                    if node.arguments is not None:
+                        for arg in node.arguments:
+                            reduce_node(arg)
+
+                if isinstance(node, javalang.tree.ConstructorDeclaration) and node.body is not None:
+                    for declaration in node.body:
+                        reduce_node(declaration)
+
+                elif isinstance(node, javalang.tree.MethodDeclaration) and node.body is not None:
+                    for statement in node.body:
+                        reduce_node(statement)
+
+                elif isinstance(node, javalang.tree.MethodInvocation) and node.arguments is not None:
+                    for arg in node.arguments:
+                        reduce_node(arg)
+
+                elif isinstance(node, javalang.tree.StatementExpression) and node.expression is not None:
+                    setattr(node, 'is_reducible', True)
+                    reduce_node(node.expression)
+
+
+                elif isinstance(node, javalang.tree.Statement):
+                    setattr(node, 'is_reducible', True)
+
+                    if isinstance(node, javalang.tree.IfStatement):
+                        reduce_node(node.then_statement)
+                        reduce_node(node.else_statement)
+
+                        else_is_reducible = True
+                        then_is_reducible = True
+
+                        if node.else_statement is not None:
+                            else_is_reducible = node.else_statement.is_reducible
+
+                        if node.then_statement is not None:
+                            then_is_reducible = node.then_statement.is_reducible
+
+                        node.is_reducible = else_is_reducible and then_is_reducible
+
+                    if isinstance(node,
+                                  (
+                                  javalang.tree.WhileStatement, javalang.tree.ForStatement, javalang.tree.DoStatement)):
+                        reduce_node(node.body)
+
+                        node.is_reducible = node.body.is_reducible
+
+                    elif isinstance(node, javalang.tree.SwitchStatement):
+                        switch_is_reducible = True
+
+                        for case in node.cases:
+                            case.is_reducible = is_case_reducible(case)
+                            switch_is_reducible = switch_is_reducible and case.is_reducible
+
+                        node.is_reducible = switch_is_reducible
+
+
+                    elif isinstance(node, javalang.tree.BlockStatement):
+                        is_block_reducible = True
+                        for statement in node.statements:
+                            if isinstance(statement, javalang.tree.Statement):
+                                reduce_node(statement)
+                                is_block_reducible = is_block_reducible and statement.is_reducible
+
+                        node.is_reducible = is_block_reducible
+
+                    elif isinstance(node, javalang.tree.TryStatement):
+                        is_try_reducible = True
+
+                        for statement in node.block:
+                            if isinstance(statement, javalang.tree.Statement):
+                                reduce_node(statement)
+                                is_try_reducible = is_try_reducible and statement.is_reducible
+
+                        if node.finally_block is not None:
+                            for statement in node.finally_block:
+                                if isinstance(statement, javalang.tree.Statement):
+                                    reduce_node(statement)
+
+                        if node.catches is not None:
+                            is_try_reducible = is_try_reducible and is_catches_reducible(node.catches)
+
+                        node.is_reducible = is_try_reducible
+
+
+                    elif isinstance(node, javalang.tree.SynchronizedStatement):
+                        if node.block is not None:
+                            for statement in node.block:
+                                if isinstance(statement, javalang.tree.Statement):
+                                    reduce_node(statement)
+
+                    elif not is_reducible_stat(node):
+                        node.is_reducible = False
+                        if isinstance(node, javalang.tree.ReturnStatement) and node.expression is not None:
+                            reduce_node(node.expression)
+
+                    else:
+                        pass
+
+        def check_red_visit_node(node):
+            nonlocal max_essential_complexity, sum_essential_complexity, decision_points
+
+            if node is not None:
+                if isinstance(node, javalang.tree.CompilationUnit):
+                    for type in node.types:
+                        check_red_visit_node(type)
+
+                if isinstance(node, javalang.tree.ClassDeclaration) and node.body is not None:
+                    for method in node.body:
+                        check_red_visit_node(method)
+
+                if isinstance(node, javalang.tree.ClassCreator):
+                    if node.body is not None:
+                        for method in node.body:
+                            check_red_visit_node(method)
+                    if node.arguments is not None:
+                        for arg in node.arguments:
+                            check_red_visit_node(arg)
+
+                elif isinstance(node, (
+                javalang.tree.MethodDeclaration, javalang.tree.ConstructorDeclaration)) and node.body is not None:
+                    sum_dp = 0
+                    for statement in node.body:
+                        decision_points = 0
+                        check_red_visit_node(statement)
+                        sum_dp += decision_points
+                    ec = sum_dp + 1
+                    if (ec == 2):
+                        ec = 1
+
+                    max_essential_complexity = max(max_essential_complexity, ec)
+                    sum_essential_complexity += ec
+
+                elif isinstance(node, javalang.tree.MethodInvocation) and node.arguments is not None:
+                    for arg in node.arguments:
+                        check_red_visit_node(arg)
+
+                elif isinstance(node, javalang.tree.StatementExpression) and node.expression is not None:
+                    check_red_visit_node(node.expression)
+
+                elif isinstance(node, javalang.tree.Statement):
+                    if is_control_stat(node) and not node.is_reducible:
+                        if isinstance(node, javalang.tree.SwitchStatement):
+                            if node.cases is not None:
+                                decision_points += len(node.cases)
+                        elif isinstance(node, javalang.tree.TryStatement):
+                            if node.catches is not None:
+                                decision_points += len(node.catches)
+                        else:
+                            decision_points += 1
+
+                    if node is not None:
+                        for attr in node.attrs:
+                            child = getattr(node, attr)
+                            if isinstance(child, javalang.ast.Node):
+                                check_red_visit_node(child)
+                            elif isinstance(child, list):
+                                for item in child:
+                                    if isinstance(item, javalang.ast.Node):
+                                        check_red_visit_node(item)
+
+        tokens = javalang.tokenizer.tokenize(java_code)
+        parser = javalang.parser.Parser(tokens)
+        tree = parser.parse()
+        reduce_node(tree)
+        check_red_visit_node(tree)
+        return max_essential_complexity, sum_essential_complexity
+
     def analyze(self):
         lizard_analysis = lizard.analyze_file.analyze_source_code(self.file_path, self.file_content)
         try:
@@ -281,9 +529,6 @@ class SoftwareMetrics:
                 self.metrics["SumCyclomaticStrict"] += lizard_function.cyclomatic_complexity
                 self.metrics["MaxCyclomaticStrict"] = max(self.metrics["MaxCyclomaticStrict"],
                                                           lizard_function.cyclomatic_complexity)
-                essential_complexity = 0
-                self.metrics["SumEssential"] += essential_complexity
-                self.metrics["MaxEssential"] = max(self.metrics["MaxEssential"], essential_complexity)
 
             self.metrics["CountLineCodeDecl"] = self.count_declarative_lines()
             self.metrics["MaxNesting"] = max(self.metrics["MaxNesting"], self.calculate_max_nesting())
@@ -291,7 +536,10 @@ class SoftwareMetrics:
             self.metrics["CountDeclFunction"] = self.count_method_declarations()
             self.metrics["CountLineCode"] = self.count_lines_of_code()
 
+            self.metrics["MaxEssential"], self.metrics["SumEssential"] = self.compute_complexity(self.file_content)
+
         except Exception as e:
             print(self.file_path, e)
 
         return self.metrics
+
