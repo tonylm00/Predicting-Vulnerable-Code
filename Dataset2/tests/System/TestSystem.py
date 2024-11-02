9f9e69d368577893702d8c6fc1be4ba5bae7dfa1
d-donia <d.donia@studenti.unisa.it>
2024-11-03 00:14:38 +0100

    Add test case 5-7 for system testing

--- Dataset2/tests/System/TestSystem.py
+++ Dataset2/tests/System/TestSystem.py
@@ -1,4 +1,5 @@
 import os
+import zipfile
 from time import sleep
 
 import pytest
@@ -31,6 +32,8 @@ class TestSystem:
     CSV_SWITCH = 27
 
     TEST_DATASET_NAME = 'test_dataset.csv'
+    TEST_ANALYSIS_NAME = 'analysis.zip'
+    TEST_PREDICTIONS_NAME = 'predictions.zip'
 
     @pytest.fixture
     def manage_environment(self, request):
@@ -63,7 +66,22 @@ class TestSystem:
 
         app.kill()
 
-        os.remove(os.path.join(test_path, self.TEST_DATASET_NAME))
+        file_path = os.path.join(test_path, self.TEST_DATASET_NAME)
+
+        if (os.path.exists(file_path)):
+            os.remove(file_path)
+
+        analyis_path = os.path.join(test_path, self.TEST_ANALYSIS_NAME)
+
+        if(os.path.exists(analyis_path)):
+            os.remove(analyis_path)
+
+        prediction_path = os.path.join(test_path, self.TEST_PREDICTIONS_NAME)
+
+        if(os.path.exists(prediction_path)):
+            os.remove(prediction_path)
+
+        os.chdir(test_path)
 
     def manage_test_dataset(self, is_empty, num_rows, is_well_formatted):
         print("manage_test")
@@ -92,45 +110,13 @@ class TestSystem:
 
         return gui_elem
 
-    def test_software_metrics(self, ):
-        # Start notepad.exe and connect to it (when a new process is spawned).
-        print(os.getcwd())
-        # Kill the app.
-
-
-        print('open windows: ', app.windows())
-
-        # Connect to the main window, modifying with title or best_match as needed
-        window = app.window(title="Perseverance")
-
-        children = window.children()
-        gui_elem = []
-        for idx, child in enumerate(children):
-            gui_elem.append(child)
-
-        list_index = [
-            self.UPLOAD_BUTTON,
-            self.SOFTWARE_METRICS_CHECK_BOX_1,
-            self.START_BUTTON,
-            self.PREDICTIONS_DOWNLOAD_BUTTON
-        ]
-
-        elem_dict = self.get_gui_elements(window, list_index)
-        upload_button = elem_dict[self.UPLOAD_BUTTON]
-        software_metrics_button = elem_dict[self.SOFTWARE_METRICS_CHECK_BOX_1]
-        start_button = elem_dict[self.START_BUTTON]
-        predict_res_button = elem_dict[self.PREDICTIONS_DOWNLOAD_BUTTON]
-
-        upload_button.click_input()
-
-        sleep(4)
-
+    def load_csv_routine(self, path):
         load_csv_app = Application().connect(title='Load CSV')
 
         load_csv_window = load_csv_app.window(title="Load CSV")
-        #load_csv_window.print_control_identifiers()
+        # load_csv_window.print_control_identifiers()
 
-        send_keys('initial_dataset.csv')
+        send_keys('test_dataset.csv')
 
         sleep(2)
 
@@ -138,7 +124,7 @@ class TestSystem:
 
         sleep(2)
 
-        send_keys('C:\\Users\\Utente\\Documents\\UNISA-MAGISTRALE\\IGES\\progetti\\Perseverance\\prova_dataset')
+        send_keys(path)
 
         sleep(2)
 
@@ -149,25 +135,16 @@ class TestSystem:
         apri_button = load_csv_window.child_window(title="&Apri")
         apri_button.click_input()
 
-        software_metrics_button.click_input()
+    def save_zip_routine(self, app, file_name, path):
+        send_keys(file_name)
 
-        start_button.click_input()
-
-        print(dir(predict_res_button))
-
-        wait_until(240, 5, predict_res_button.is_visible, True)
-
-        predict_res_button.click_input()
-
-        send_keys('results')
-
-        sleep(3)
+        sleep(2)
 
         send_keys('^l')
 
         sleep(2)
 
-        send_keys('C:\\Users\\Utente\\PycharmProjects\\Predicting-Vulnerable-Code\\Dataset2\\tests\\System')
+        send_keys(path)
 
         sleep(2)
 
@@ -180,6 +157,25 @@ class TestSystem:
 
         salva_button = save_zip_window.child_window(title="&Salva", class_name="Button")
         salva_button.click_input()
+
+        sleep(5)
+
+        success_dialog = app.Dialog
+
+        exist_dialog = app.Dialog.exist
+
+        success_text = app.Dialog.Static2.window_text()
+
+        success_dialog.OK.click_input()
+
+        sleep(2)
+
+        file_path = os.path.join(path, file_name)
+
+        # zip file handler
+        zip = zipfile.ZipFile(file_path)
+
+        return exist_dialog, success_text, zip
 
     @pytest.mark.parametrize('manage_environment', [(False, False, 0, False)], indirect=True)
     def test_case_1(self, manage_environment):
@@ -212,11 +208,6 @@ class TestSystem:
         # Connect to the main window, modifying with title or best_match as needed
         window = app.window(title="Perseverance")
 
-        children = window.children()
-        gui_elem = []
-        for idx, child in enumerate(children):
-            gui_elem.append(child)
-
         list_index = [
             self.UPLOAD_BUTTON,
             self.SOFTWARE_METRICS_CHECK_BOX_1,
@@ -232,29 +223,7 @@ class TestSystem:
 
         sleep(4)
 
-        load_csv_app = Application().connect(title='Load CSV')
-
-        load_csv_window = load_csv_app.window(title="Load CSV")
-        # load_csv_window.print_control_identifiers()
-
-        send_keys('test_dataset.csv')
-
-        sleep(2)
-
-        send_keys('^l')
-
-        sleep(2)
-
-        send_keys(test_path)
-
-        sleep(2)
-
-        send_keys('{ENTER}')
-
-        sleep(2)
-
-        apri_button = load_csv_window.child_window(title="&Apri")
-        apri_button.click_input()
+        self.load_csv_routine(test_path)
 
         sleep(2)
 
@@ -275,17 +244,13 @@ class TestSystem:
         # Connect to the main window, modifying with title or best_match as needed
         window = app.window(title="Perseverance")
 
-        children = window.children()
-        gui_elem = []
-        for idx, child in enumerate(children):
-            gui_elem.append(child)
-
         list_index = [
             self.UPLOAD_BUTTON,
             self.SOFTWARE_METRICS_CHECK_BOX_1,
             self.START_BUTTON,
             self.TEXT_MINING_CHECK_BOX,
-            self.PREDICTIONS_DOWNLOAD_BUTTON
+            self.PREDICTIONS_DOWNLOAD_BUTTON,
+            self.ANALYSIS_DOWNLOAD_BUTTON
         ]
 
         elem_dict = self.get_gui_elements(window, list_index)
@@ -293,37 +258,17 @@ class TestSystem:
         software_metrics_box = elem_dict[self.SOFTWARE_METRICS_CHECK_BOX_1]
         text_mining_box = elem_dict[self.TEXT_MINING_CHECK_BOX]
         start_button = elem_dict[self.START_BUTTON]
+        analysis_download_button = elem_dict[self.ANALYSIS_DOWNLOAD_BUTTON]
         predict_res_button = elem_dict[self.PREDICTIONS_DOWNLOAD_BUTTON]
 
         upload_button.click_input()
 
-        sleep(4)
+        sleep(3)
 
-        load_csv_app = Application().connect(title='Load CSV')
-
-        load_csv_window = load_csv_app.window(title="Load CSV")
-        # load_csv_window.print_control_identifiers()
-
-        send_keys('test_dataset.csv')
+        self.load_csv_routine(test_path)
 
         sleep(2)
 
-        send_keys('^l')
-
-        sleep(2)
-
-        send_keys(test_path)
-
-        sleep(2)
-
-        send_keys('{ENTER}')
-
-        sleep(2)
-
-        apri_button = load_csv_window.child_window(title="&Apri")
-        apri_button.click_input()
-
-        sleep(2)
 
         software_metrics_box.click_input()
         text_mining_box.click_input()
@@ -332,33 +277,105 @@ class TestSystem:
 
         start_button.click_input()
 
-        sleep(4)
-
         wait_until(240, 5, predict_res_button.is_visible, True)
 
+        analysis_download_button.click_input()
+
+        sleep(2)
+
+        exist_analysis_success_dialog, analysis_success_text, analysis_zip = self.save_zip_routine(app, self.TEST_ANALYSIS_NAME,
+                                                                                                   test_path)
         predict_res_button.click_input()
 
-        send_keys('results.zip')
+        sleep(2)
+
+        exist_prediction_success_dialog, prediction_success_text, prediction_zip = self.save_zip_routine(app, self.TEST_PREDICTIONS_NAME, test_path)
+
+        assert exist_analysis_success_dialog
+        assert 'File saved successfully' in analysis_success_text
+        assert os.path.exists(os.path.join(test_path, self.TEST_ANALYSIS_NAME))
+        assert analysis_zip.namelist() == ['repo_mining.log', 'csv_mining_final.csv', 'mining_results_sm_final.csv',
+                                           'Union_TM_SM.csv']
+
+        assert exist_prediction_success_dialog
+        assert 'File saved successfully' in prediction_success_text
+        assert os.path.exists(os.path.join(test_path, self.TEST_PREDICTIONS_NAME))
+        assert len(prediction_zip.namelist())==0
+
+    @pytest.mark.parametrize('manage_environment', [(True, False, 0, True)], indirect=True)
+    def test_case_7(self, manage_environment):
+
+        app, test_path = manage_environment
+
+        # Connect to the main window, modifying with title or best_match as needed
+        window = app.window(title="Perseverance")
+
+        list_index = [
+            self.UPLOAD_BUTTON,
+            self.SOFTWARE_METRICS_CHECK_BOX_1,
+            self.START_BUTTON,
+            self.TEXT_MINING_CHECK_BOX,
+            self.PREDICTIONS_DOWNLOAD_BUTTON,
+            self.ANALYSIS_DOWNLOAD_BUTTON
+        ]
+
+        elem_dict = self.get_gui_elements(window, list_index)
+        upload_button = elem_dict[self.UPLOAD_BUTTON]
+        software_metrics_box = elem_dict[self.SOFTWARE_METRICS_CHECK_BOX_1]
+        text_mining_box = elem_dict[self.TEXT_MINING_CHECK_BOX]
+        start_button = elem_dict[self.START_BUTTON]
+        analysis_download_button = elem_dict[self.ANALYSIS_DOWNLOAD_BUTTON]
+        predict_res_button = elem_dict[self.PREDICTIONS_DOWNLOAD_BUTTON]
+
+        upload_button.click_input()
 
         sleep(3)
 
-        send_keys('^l')
+        self.load_csv_routine(test_path)
 
         sleep(2)
 
-        send_keys(test_path)
+        text_mining_box.click_input()
 
         sleep(2)
 
-        send_keys('{ENTER}')
+        start_button.click_input()
 
-        save_zip_app = Application().connect(title='Save ZIP file as')
+        wait_until(240, 5, predict_res_button.is_visible, True)
 
-        save_zip_window = save_zip_app.window(title="Save ZIP file as")
-        save_zip_window.print_control_identifiers()
+        analysis_download_button.click_input()
 
-        salva_button = save_zip_window.child_window(title="&Salva", class_name="Button")
-        salva_button.click_input()
+        sleep(2)
+
+        exist_analysis_success_dialog, analysis_success_text, analysis_zip = self.save_zip_routine(app, self.TEST_ANALYSIS_NAME,
+                                                                                                   test_path)
+        predict_res_button.click_input()
+
+        sleep(2)
+
+        exist_prediction_success_dialog, prediction_success_text, prediction_zip = self.save_zip_routine(app, self.TEST_PREDICTIONS_NAME, test_path)
+
+        assert exist_analysis_success_dialog
+        assert 'File saved successfully' in analysis_success_text
+        assert os.path.exists(os.path.join(test_path, self.TEST_ANALYSIS_NAME))
+        assert analysis_zip.namelist() == ['repo_mining.log', 'csv_mining_final.csv']
+
+        assert exist_prediction_success_dialog
+        assert 'File saved successfully' in prediction_success_text
+        assert os.path.exists(os.path.join(test_path, self.TEST_PREDICTIONS_NAME))
+        assert len(prediction_zip.namelist())==0
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
